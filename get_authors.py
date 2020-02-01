911ca28be924070ce71cd2525f3fd3347bc16f90
Javier López <javierlopez@uc.cl>
2020-02-01 16:00:26 -0300

    lint: authors

--- get_authors.py
+++ get_authors.py
@@ -11,4 +11,4 @@ authors = list(set(map(lambda b: b["author"], books)))
 print(colored("{} unique authors in total!".format(len(authors)), 'green', attrs=["bold"]))
 
 with open("books/_authors.json", "w") as f:
-	json.dump({"authors": authors}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
+    json.dump({"authors": authors}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)

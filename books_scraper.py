fc26fe88f1a2407dc7a3e96d9642b4af67f68b87
Javier López <javierlopez@uc.cl>
2020-02-01 15:59:23 -0300

    refactor(scraper): convert to class

--- books_scraper.py
+++ books_scraper.py
@@ -9,10 +9,10 @@ import json
 import os.path
 import traceback
 
+from termcolor import colored
+
 from dotenv import load_dotenv
 load_dotenv()
-
-from termcolor import colored
 
 BASE_URL = "https://www.goodreads.com"
 COOKIE = os.getenv("SESSION_ID")
@@ -20,119 +20,130 @@ COOKIE = os.getenv("SESSION_ID")
 PAGES_PER_SHELF = int(os.getenv("PAGES_PER_SHELF"))
 # PAGES_PER_SHELF = 3
 
+
 def print_book(book):
-	print("-"*30)
-	print(book["title"])
-	print(book["isbn"])
-	# print(book["author"])
-	# print(book["description"])
-	# print(book["date_published"])
-	# print(book["publisher"])
-	# print(book["img_url"])
-	# print(book["rating_count"])
-	# print(book["rating_average"])
-	# print(book["genres"])
-	# print(book["book_format"])
-	# print(book["pages"])
-	# print(book["language"])
-	# print(len(book["reviews"]))
+    print("-" * 30)
+    print(book["title"])
+    print(book["isbn"])
 
-def scrap_book(url):
-	try:
-		source_book = requests.get(url, timeout=5)
-		soup_book = bs.BeautifulSoup(source_book.content, features="html.parser")
-		metacol = soup_book.find(id="metacol")
 
-		isbn = soup_book.find("meta", {"property":"books:isbn"}).get("content")
-		if isbn == "null":
-			raise Exception("Null ISBN error")
+class GoodreadsScraper():
+    def __init__(self):
+        self.shelves = []
 
-		title = metacol.find(id="bookTitle").text.strip()
-		author = metacol.find(class_="authorName").text.strip()
+    def load_shelves(self):
+        with open("shelves.txt", "r") as f:
+            self.shelves = f.read().splitlines()
 
-		description_div = metacol.find(id="description")
-		description = description_div.find_all("span")[-1].text.strip() if description_div else ""
+    def scrap_shelf(self, i, shelf):
+        print(colored("Started - {} - page {}".format(shelf, i), 'yellow', attrs=['bold']))
 
-		img = soup_book.find(id="coverImage")
-		img_url = img.get("src") if img else ""
+        if os.path.isfile("books/{}_{}.json".format(shelf, i)):
+            print(colored("Finished - {} - page {} - already exists...".format(shelf, i), "green", attrs=['bold']))
+            return -1
 
-		rating_count = metacol.find("meta", {"itemprop":"ratingCount"}).get("content")
-		rating_average = metacol.find("span", {"itemprop":"ratingValue"}).text
+        shelf_url = BASE_URL + "/shelf/show/{}?page={}".format(shelf, i)
+        headers = {"Cookie": COOKIE}
+        try:
+            source = requests.get(shelf_url, timeout=5, headers=headers)
+        except Exception:
+            return 0
+        soup = bs.BeautifulSoup(source.content, features="html.parser")
 
-		pages = soup_book.find("meta", {"property":"books:page_count"}).get("content")
+        books_urls = []
+        for elem in soup.find_all(class_="bookTitle"):
+            url = elem.get("href")
+            books_urls.append(BASE_URL + url)
+        books = Parallel(n_jobs=1)(delayed(self.scrap_book)(book_url) for book_url in books_urls)
+        books = list(filter(lambda x: x is not None, books))
 
-		details = metacol.find(id="details")
+        with open("books/{}_{}.json".format(shelf, i), "w") as f:
+            json.dump(
+                {"books": books},
+                f,
+                indent=4,
+                separators=(',', ': '),
+                sort_keys=True,
+                ensure_ascii=False
+            )
 
-		book_format_div = details.find("span", {"itemprop":"bookFormat"}) if details else None
-		book_format = book_format_div.text if book_format_div else ""
+        print(colored("Finished - {} - page {} with {}/{} books".format(shelf, i, len(books), len(books_urls)), 'green', attrs=['bold']))
+        return len(books)
 
-		language_div = details.find("div", {"itemprop":"inLanguage"}) if details else None
-		language = language_div.text if language_div else ""
+    def scrap_book(self, book_url):
+        try:
+            source_book = requests.get(book_url, timeout=5)
+            soup_book = bs.BeautifulSoup(source_book.content, features="html.parser")
 
-		publication = details.find_all(class_="row")[1].text.strip() if details else ""
-		date_published = publication.split("\n")[1].strip()	if publication else ""
-		publisher = publication.split("\n")[2].strip()[3:] if publication else ""
+            isbn = soup_book.find("meta", {"property": "books:isbn"}).get("content")
+            if isbn == "null":
+                raise Exception("Null ISBN error")
 
-		genres = soup_book.find_all(class_="actionLinkLite bookPageGenreLink")
-		genres = [genre.text for genre in genres]
+            metacol = soup_book.find(id="metacol")
+            title = metacol.find(id="bookTitle").text.strip()
+            author = metacol.find(class_="authorName").text.strip()
 
-		reviews_divs = soup_book.find_all(class_="reviewText stacked")
-		reviews = [review_div.find("span").find_all("span")[-1].text for review_div in reviews_divs]
-		book = {
-			"title": title,
-			"author": author,
-			"description": description,
-			"img_url": img_url,
-			"isbn": str(isbn),
-			"rating_count": int(rating_count),
-			"rating_average": float(rating_average),
-			"date_published": str(date_published),
-			"publisher": publisher,
-			"genres": genres,
-			"book_format": book_format,
-			"pages": int(pages),
-			"language": language,
-			"goodreads_url": url,
-			"reviews": reviews
-		}
-		print_book(book)
-		return book
+            description_div = metacol.find(id="description")
+            description = description_div.find_all("span")[-1].text.strip() if description_div else ""
 
-	except Exception as e:
-		print("-"*30)
-		print(colored("ERROR: {}\n{}URL: {}".format(e, traceback.format_exc(), url), 'red'))
-		return
+            img = soup_book.find(id="coverImage")
+            img_url = img.get("src") if img else ""
 
-def scrap(i, shelf):
-	print(colored("Started - {} - page {}".format(shelf, i), 'yellow', attrs=['bold']))
+            rating_count = metacol.find("meta", {"itemprop": "ratingCount"}).get("content")
+            rating_average = metacol.find("span", {"itemprop": "ratingValue"}).text
 
-	if os.path.isfile("books/{}_{}.json".format(shelf, i)):
-		print(colored("Finished - {} - page {} - already exists...".format(shelf, i), "green", attrs=['bold']))
-		return -1
+            pages = soup_book.find("meta", {"property": "books:page_count"}).get("content")
 
-	page_url = BASE_URL + "/shelf/show/{}?page={}".format(shelf, i)
-	headers = {"Cookie":COOKIE}
-	try:
-		source = requests.get(page_url, timeout=5, headers=headers)
-	except:
-		return 0
-	soup = bs.BeautifulSoup(source.content, features="html.parser") 
-	
-	books_urls = []
-	for elem in soup.find_all(class_="bookTitle"):
-		url = elem.get("href")
-		books_urls.append(BASE_URL + url)
-	books = Parallel(n_jobs=1)(delayed(scrap_book)(book_url) for book_url in books_urls)
-	books = list(filter(lambda x: x != None, books))
+            details = metacol.find(id="details")
 
-	with open("books/{}_{}.json".format(shelf, i), "w") as f:
-		json.dump({"books": books}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
+            book_format_div = details.find("span", {"itemprop": "bookFormat"}) if details else None
+            book_format = book_format_div.text if book_format_div else ""
 
-	print(colored("Finished - {} - page {} with {}/{} books".format(shelf, i, len(books), len(books_urls)), 'green', attrs=['bold']))
-	return len(books)
+            language_div = details.find("div", {"itemprop": "inLanguage"}) if details else None
+            language = language_div.text if language_div else ""
 
-START = 1
-END = PAGES_PER_SHELF
-with open("shelves.txt", "r") as f:
-	shelves = f.read().splitlines()
-results = Parallel(n_jobs=cpu_count())(delayed(scrap)(i, shelf) for shelf in shelves for i in range(START, END + 1))
+            publication = details.find_all(class_="row")[1].text.strip() if details else ""
+            date_published = publication.split("\n")[1].strip() if publication else ""
+            publisher = publication.split("\n")[2].strip()[3:] if publication else ""
+
+            genres = soup_book.find_all(class_="actionLinkLite bookPageGenreLink")
+            genres = [genre.text for genre in genres]
+
+            reviews_divs = soup_book.find_all(class_="reviewText stacked")
+            reviews = [review_div.find("span").find_all("span")[-1].text for review_div in reviews_divs]
+            book = {
+                "title": title,
+                "author": author,
+                "description": description,
+                "img_url": img_url,
+                "isbn": str(isbn),
+                "rating_count": int(rating_count),
+                "rating_average": float(rating_average),
+                "date_published": str(date_published),
+                "publisher": publisher,
+                "genres": genres,
+                "book_format": book_format,
+                "pages": int(pages),
+                "language": language,
+                "goodreads_url": book_url,
+                "reviews": reviews
+            }
+            print_book(book)
+            return book
+
+        except Exception as e:
+            print("-" * 30)
+            print(colored("ERROR: {}\n{}URL: {}".format(e, traceback.format_exc(), book_url), 'red'))
+            return
+
+    def run(self):
+        self.load_shelves()
+        Parallel(n_jobs=cpu_count())(
+            delayed(self.scrap_shelf)(i, shelf)
+            for shelf in self.shelves
+            for i in range(1, PAGES_PER_SHELF + 1)
+        )
+
+
+scraper = GoodreadsScraper()
+scraper.run()
